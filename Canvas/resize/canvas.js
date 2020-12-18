dea7d95eaa912da7b0ae5d8e493e6bc9994c4d4d
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-12-18 12:14:34 -0300

    drawing bouncing circle

--- Canvas/resize/canvas.js
+++ Canvas/resize/canvas.js
@@ -4,20 +4,20 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
-c.fillStyle = 'rgba(255, 0, 0, 0.5)';
-c.fillRect(100, 100, 100, 100);
-c.fillStyle = 'rgba(0, 255, 0, 0.5)';
-c.fillRect(400, 100, 100, 100);
-c.fillStyle = 'rgba(0, 0, 255, 0.5)';
-c.fillRect(300, 300, 100, 100);
-console.log(canvas)
+// c.fillStyle = 'rgba(255, 0, 0, 0.5)';
+// c.fillRect(100, 100, 100, 100);
+// c.fillStyle = 'rgba(0, 255, 0, 0.5)';
+// c.fillRect(400, 100, 100, 100);
+// c.fillStyle = 'rgba(0, 0, 255, 0.5)';
+// c.fillRect(300, 300, 100, 100);
+// console.log(canvas)
 
-//Line
-c.beginPath();
-c.moveTo(50, 300);
-c.lineTo(300, 100);
-c.lineTo(400, 300)
-c.stroke();
+// //Line
+// c.beginPath();
+// c.moveTo(50, 300);
+// c.lineTo(300, 100);
+// c.lineTo(400, 300)
+// c.stroke();
 
 // Arc / Circle
 // c.beginPath();
@@ -25,11 +25,31 @@ c.stroke();
 // c.strokeStyle = 'blue';
 // c.stroke();
 
-for (var i = 0; i < 3; i++) {
-    var x = Math.random() * window.innerWidth;
-    var y = Math.random() * window.innerHeight;
+// for (var i = 0; i < 3; i++) {
+//     var x = Math.random() * window.innerWidth;
+//     var y = Math.random() * window.innerHeight;
+//     c.beginPath();
+//     c.arc(x, y, 30, 0, Math.PI*2, false);
+//     c.strokeStyle = 'blue';
+//     c.stroke();
+// }
+
+
+let x = 200;
+let dx = 8;
+let radius = 30;
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
     c.beginPath();
-    c.arc(x, y, 30, 0, Math.PI*2, false);
+    c.arc(x, 300, radius, 0, Math.PI*2, false);
     c.strokeStyle = 'blue';
     c.stroke();
+    
+    if(x + radius > innerWidth || x - radius < 0) {
+        dx = -dx;
+    } 
+    x += dx;
 }
+
+animate();
