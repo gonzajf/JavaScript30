5bd3bd6b33b3eec78599c44fb8585a0895d89506
Gonzalo Franchino <gonzalo.franchino@gmail.com>
2020-12-19 13:18:14 -0300

    drawing bouncing circle in x and y

--- Canvas/resize/canvas.js
+++ Canvas/resize/canvas.js
@@ -36,20 +36,27 @@ var c = canvas.getContext('2d');
 
 
 let x = 200;
+let y = 200
 let dx = 8;
+let dy = 6;
 let radius = 30;
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
     c.beginPath();
-    c.arc(x, 300, radius, 0, Math.PI*2, false);
+    c.arc(x, y, radius, 0, Math.PI*2, false);
     c.strokeStyle = 'blue';
     c.stroke();
     
     if(x + radius > innerWidth || x - radius < 0) {
         dx = -dx;
-    } 
+    }
+    
+    if(y + radius > innerHeight || y - radius < 0) {
+        dy = -dy;
+    }
     x += dx;
+    y += dy;
 }
 
 animate();
